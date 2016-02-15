cad90f7f3e4f204adfb222a2df32c9befdbbc96e
Hussein <debelhussein@gmail.com>
2016-02-15 19:12:05 +1100

    Playing around with java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args){
+        // int x = 3;
+        // String name = "Hussien";
+        // char letter = 'a';
+        // System.out.println(x + name + letter); // System.out.println(x, name, letter) doesnt work
+        // double d = Math.random();
+        // System.out.println("The random number is " + d);
+
+    }
+}
