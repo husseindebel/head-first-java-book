22600d6f29952e1a4cc1257ccb3382fa789ee117
Hussein <debelhussein@gmail.com>
2016-02-15 19:18:05 +1100

    more practice

--- hello.java
+++ hello.java
@@ -6,6 +6,13 @@ public class hello {
         // System.out.println(x + name + letter); // System.out.println(x, name, letter) doesnt work
         // double d = Math.random();
         // System.out.println("The random number is " + d);
-
+        int count = 0;
+        while (count < 10){
+            count++;
+            System.out.println(count);
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("I am at number "+i);
+        }
     }
 }
