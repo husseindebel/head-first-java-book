a3b1a4049470cce93eda0956a9834d7a61ec526d
Hussein <debelhussein@gmail.com>
2016-02-16 15:15:59 +1100

    Added comments explaining why the code is bad

--- Guessing_Game/GuessGame.java
+++ Guessing_Game/GuessGame.java
@@ -1,6 +1,6 @@
 // create the player objects
 // run the game
-// crappy version
+// crappy version (why? i explain below)
 
 public class GuessGame {
     // instance variables (what does this class know? It knows the players)
@@ -11,26 +11,26 @@ public class GuessGame {
     // method (what does this class do? It starts the game)
     public void startGame(){
         // create the player objects
-        p1 = new Player();
+        p1 = new Player();  // I think i can abstract this out as another method
         p2 = new Player();
         p3 = new Player();
 
         // hold the guesses for each player
-        int guessp1 = 0;
-        int guessp2 = 0;
+        int guessp1 = 0;  // this is not the GuessGames job to manage
+        int guessp2 = 0;  // should belong to the player objects
         int guessp3 = 0;
 
         // hold the players correctness state
-        boolean p1isRight = false;
-        boolean p2isRight = false;
+        boolean p1isRight = false; // same as the previous block
+        boolean p2isRight = false; // should belong to the player object
         boolean p3isRight = false;
 
-        int targetNumber = (int) (Math.random()*10);
+        int targetNumber = (int) (Math.random()*10); // this can be abstracted out
         System.out.println("I am thinking of a number between 0 and 9");
 
         while (true){
             System.out.println("Number to guess is " + targetNumber);
-            p1.guess();
+            p1.guess();  // should be handled when the player object is created
             p2.guess();
             p3.guess();
 
@@ -41,6 +41,7 @@ public class GuessGame {
             guessp3 = p3.number;
             System.out.println("Player three guessed "+ guessp3);
 
+            // can be abstracted out
             if (guessp1 == targetNumber){
                 p1isRight = true;
             } else if (guessp2 == targetNumber){
@@ -48,7 +49,7 @@ public class GuessGame {
             } else if (guessp3 == targetNumber){
                 p3isRight = true;
             }
-
+            
             if (p1isRight || p2isRight || p3isRight){
                 System.out.println("We have a winner");
                 System.out.println("Player one got it right? " + p1isRight);
