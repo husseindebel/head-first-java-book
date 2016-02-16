eb0ebd3fded342de186d64ea0b8e9fa2d34336ba
Hussein <debelhussein@gmail.com>
2016-02-16 15:00:44 +1100

    Guessing game started

---
+++ Guessing_Game/GuessGame.java
@@ -0,0 +1,30 @@
+// create the player objects
+// run the game
+// crappy version
+
+public class GuessGame {
+    // instance variables (what does this class know? It knows the players)
+    Player p1;
+    Player p2;
+    Player p3;
+
+    // method (what does this class do? It starts the game)
+    public void startGame(){
+        // create the player objects
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+
+        // hold the guesses for each player
+        int guessp1 = 0;
+        int guessp2 = 0;
+        int guessp3 = 0;
+
+        
+        boolean p1isRight = false;
+        boolean p2isRight = false;
+        boolean p3isRight = false;
+
+
+    }
+}
